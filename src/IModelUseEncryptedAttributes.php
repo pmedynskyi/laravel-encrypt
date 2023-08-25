bb7d5c7fe3184625ee132ef6d49085e6f17ac782
pmedynskyi <85118993+pmedynskyi@users.noreply.github.com>
2023-08-25 17:28:52 +0200

    Update IModelUseEncryptedAttributes.php
    fix interface typo

--- src/IModelUseEncryptedAttributes.php
+++ src/IModelUseEncryptedAttributes.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Pmedynskyi\LaravelEncrypt;
 
-interface IModelUseEncryptedAttribute
+interface IModelUseEncryptedAttributes
 {
     public function encryptedAttribute(): Attribute;
 }
